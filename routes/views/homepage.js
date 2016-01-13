cd2195f8fa41a72ed0c64d279e1ac3d795049f26
Steve Laing <developer@laingsolutions.com>
2016-01-13 22:28:50 +0000

    Initial site...
    This includes a working keystone 0.3.17 site with content enhancements
    and some amazing design and artwork.

---
+++ routes/views/homepage.js
@@ -0,0 +1,31 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.section = 'home';
+
+  locals.data = {
+    content : {}
+  };
+
+  view.on('init', function(next) {
+
+    var content = keystone.list('Content').model.findBySlug('home')
+      .exec(function(err, results) {
+        if (err || !results.length) {
+          next(err);
+        } else {
+          locals.data.content = results[0];
+          next();
+        }
+      });
+  });
+
+  // Render the view
+  view.render('homepage', locals.data.content);
+};
